53be372cff4c1a612ff6df381a0c555468a5cbd6
dutenrapha <dutenrapha@hotmail.com>
2020-09-17 13:53:54 -0300

    Teste cena no escuro com a iluminação de vela

---
+++ Puzzle-Rush/Assets/moveCandle.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveCandle : MonoBehaviour {
+	
+	public GameObject crosshairs;
+    private Vector3 target;
+
+	 void Update () {
+        target = transform.GetComponent<Camera>().ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z));
+        crosshairs.transform.position = new Vector2(target.x, target.y);
+	 }
+}
